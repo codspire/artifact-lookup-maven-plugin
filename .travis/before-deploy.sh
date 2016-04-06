748302fa2cdb4f773a23acb34460d91e9763289a
codspire <codspire@gmail.com>
2016-04-06 03:34:33 -0400

    trying oss deployment

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -1,5 +1,12 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash -x
+
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then    
-    openssl aes-256-cbc -K $encrypted_ca544fcb7638_key -iv $encrypted_ca544fcb7638_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d    
-    gpg --fast-import .travis/codesigning.asc
+    
+    ls -ltr .travis/*
+    
+    openssl aes-256-cbc -K $encrypted_ca544fcb7638_key -iv $encrypted_ca544fcb7638_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
+        
+    ls -ltr .travis/*
+    
+    gpg -v --fast-import .travis/codesigning.asc
 fi
