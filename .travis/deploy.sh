b33c8583162afc592070bf425aebe0bcc154a184
codspire <codspire@gmail.com>
2016-04-06 02:52:47 -0400

    trying oss deployment

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
+
 # Thanks to https://github.com/DracoBlue/http-response-headers/blob/master/.travis/deploy.sh
+# https://github.com/blackdoor/hate/tree/master/cd
 cd `dirname $0`/.. 
 
-if [ -z "$SONATYPE_USERNAME" ]
+if [ -z "$OSSRH_JIRA_USERNAME" ]
 then
-    echo "error: please set SONATYPE_USERNAME and SONATYPE_PASSWORD environment variable"
+    echo "error: please set OSSRH_JIRA_USERNAME and OSSRH_JIRA_PASSWORD environment variable"
     exit 1
 fi
 
-if [ -z "$SONATYPE_PASSWORD" ]
+if [ -z "$OSSRH_JIRA_PASSWORD" ]
 then
-    echo "error: please set SONATYPE_PASSWORD environment variable"
+    echo "error: please set OSSRH_JIRA_PASSWORD environment variable"
     exit 1
 fi
 
-if [ ! -z "$TRAVIS_TAG" ]
-then
-    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
-else
-    echo "not on a tag -> keep snapshot version in pom.xml"
-fi
+#if [ ! -z "$TRAVIS_TAG" ]
+#then
+#    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
+#    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+#else
+#    echo "not on a tag -> keep snapshot version in pom.xml"
+#fi
 
-mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
+mvn clean deploy -DskipTests=true -P sign,build-extras --settings .travis/settings.xml -B -U
