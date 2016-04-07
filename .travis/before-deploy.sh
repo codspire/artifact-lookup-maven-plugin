46c54b2221e3220309ab05e30cc574161e4a16e6
codspire <codspire@gmail.com>
2016-04-06 21:09:04 -0400

    updated openssl keys

--- .travis/before-deploy.sh
+++ .travis/before-deploy.sh
@@ -2,11 +2,11 @@
 
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then    
     
-    ls -ltr .travis/*
+	ls -ltr .travis/*
+  
+	openssl aes-256-cbc -K $encrypted_6415f14baeb5_key -iv $encrypted_6415f14baeb5_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d      
+  
+	ls -ltr .travis/*
     
-    openssl aes-256-cbc -K $encrypted_ca544fcb7638_key -iv $encrypted_ca544fcb7638_iv -in .travis/codesigning.asc.enc -out .travis/codesigning.asc -d
-        
-    ls -ltr .travis/*
-    
-    gpg -v --fast-import .travis/codesigning.asc
+	gpg -v --fast-import .travis/codesigning.asc
 fi
