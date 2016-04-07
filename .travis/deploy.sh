84df566818ba695a7477ee3c5efeea52036fc514
codspire <codspire@gmail.com>
2016-04-06 22:13:31 -0400

    added maven central badge, added tag -> release version

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -16,12 +16,12 @@ then
     exit 1
 fi
 
-#if [ ! -z "$TRAVIS_TAG" ]
-#then
-#    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-#    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
-#else
-#    echo "not on a tag -> keep snapshot version in pom.xml"
-#fi
+if [ ! -z "$TRAVIS_TAG" ]
+then
+    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.1:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+else
+    echo "not on a tag -> keep snapshot version in pom.xml"
+fi
 
 mvn deploy -DskipTests=true -P sign,build-extras --settings .travis/settings.xml -B -U
